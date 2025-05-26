fbd67e317af1fd8562c0d315a47887eef7d4a195
c++junkie <asolinks@hotmail.com>
2025-05-26 21:02:05 +0300

    merge sorted array solved

---
+++ merge_sorted_array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>  // for std::sort
+#include <cassert>    // for assert
+
+class Solution {
+public:
+    void merge(std::vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = 0; i < n; ++i){
+        	nums1[m+i] = nums2[i];        	
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
