35c44bd186f5a45e021cc94b00d501410a657d57
c++junkie <asolinks@hotmail.com>
2025-05-27 23:55:37 +0300

    compiled

--- length_of_last_word.cpp
+++ length_of_last_word.cpp
@@ -5,7 +5,7 @@
 
 class Solution {
 public:
-    int lengthOfLastWord(string s) {
+    int lengthOfLastWord(std::string s) {
         int length = 0;
         int i = s.length() - 1;
 
@@ -23,3 +23,17 @@ public:
         return length;
     }
 };
+
+int main() {
+    Solution solution;
+
+    std::string test1 = "Hello World";
+    std::string test2 = "   fly me   to   the moon  ";
+    std::string test3 = "luffy is still joyboy";
+
+    std::cout << "Test 1: " << solution.lengthOfLastWord(test1) << std::endl;  // Output: 5
+    std::cout << "Test 2: " << solution.lengthOfLastWord(test2) << std::endl;  // Output: 4
+    std::cout << "Test 3: " << solution.lengthOfLastWord(test3) << std::endl;  // Output: 6
+
+    return 0;
+}
