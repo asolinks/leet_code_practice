081eaa0a00a2c4950266c67fc5ef585955678a18
c++junkie <asolinks@hotmail.com>
2025-05-28 18:19:57 +0300

    rotate code modified, bugs sorted

--- rotate_array.cpp
+++ rotate_array.cpp
@@ -6,6 +6,14 @@
 class Solution {
 public:
     void rotate(std::vector<int>& nums, int k) {
-        
+        int n = nums.size();
+        k %= n;
+
+        std::vector<int> temp (n);
+        for (int i=0; i < n; ++i){
+            temp[(i + k) % n] = nums[i];
+        }
+        nums = temp;
+
     }
 };
