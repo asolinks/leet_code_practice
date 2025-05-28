1eb32e74334582900018c3eb575275246104eb30
c++junkie <asolinks@hotmail.com>
2025-05-28 17:26:45 +0300

    rotate array

---
+++ rotate_array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>  // for std::sort
+#include <cassert>    // for assert
+
+class Solution {
+public:
+    void rotate(std::vector<int>& nums, int k) {
+        
+    }
+};
