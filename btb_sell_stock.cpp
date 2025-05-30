1b22e5afd4388e924860f4345c8ac822d598cab7
c++junkie <asolinks@hotmail.com>
2025-05-30 22:01:48 +0300

    best time to buy and sell stock exercise updated

--- btb_sell_stock.cpp
+++ btb_sell_stock.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<vector>
-#include <algorithm>  // for std::sort
-#include <cassert>    // for assert
+#include <climits> 
 
 class Solution {
 public:
@@ -19,3 +18,20 @@ public:
 		return maxProfit;
 	}
 };
+
+int main() {
+    Solution solution;
+
+    std::vector<int> prices1 = {7, 1, 5, 3, 6, 4};
+    std::vector<int> prices2 = {7, 6, 4, 3, 1};
+
+    std::cout << "Test case 1: ";
+    for (int price : prices1) std::cout << price << " ";
+    std::cout << "\nMax Profit: " << solution.maxProfit(prices1) << std::endl;
+
+    std::cout << "Test case 2: ";
+    for (int price : prices2) std::cout << price << " ";
+    std::cout << "\nMax Profit: " << solution.maxProfit(prices2) << std::endl;
+
+    return 0;
+}
