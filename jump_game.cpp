05dd6e752c63457decc416f51d922efa5cf90cdf
c++junkie <asolinks@hotmail.com>
2025-06-02 23:54:53 +0300

    Jump Game Exercise main function added for testing

--- jump_game.cpp
+++ jump_game.cpp
@@ -13,3 +13,20 @@ public:
         
     }
 };
+
+int main() {
+    // Example input
+    std::vector<int> nums = {2, 3, 1, 1, 4}; // Change this for other test cases
+
+    Solution solution;
+    bool result = solution.canJump(nums);
+
+    // Print the result
+    if (result) {
+        std::cout << "Can jump to the end!" << std::endl;
+    } else {
+        std::cout << "Cannot jump to the end." << std::endl;
+    }
+
+    return 0;
+}
