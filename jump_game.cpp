d05f5ff3906aca85b3c4a73a594cabb7031ae8a8
c++junkie <asolinks@hotmail.com>
2025-06-02 23:50:56 +0300

    Jump Game Exercise solved

--- jump_game.cpp
+++ jump_game.cpp
@@ -4,6 +4,12 @@
 class Solution {
 public:
     bool canJump(std::vector<int>& nums) {
+    	int farthest = 0;
+    	for(int = i; i < nums.size(); ++i){
+    		if (i >) return false;
+    		farthest = std::max(farthest, i +nums[i]);
+    	}
+    	return true;
         
     }
 };
