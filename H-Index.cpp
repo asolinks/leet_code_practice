57a5a24543d26402397ea8c7c0c3937a759e95dc
c++junkie <asolinks@hotmail.com>
2025-06-04 21:26:05 +0300

    H-Index Exercise Solved

---
+++ H-Index.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    int hIndex(std::vector<int>& citations) {
+    	std::sort(citations.begin(), citations.end(), std::greater<int>());
+    	int h = 0;
+
+    	for (int i = 0; i < citations.size(); ++i){
+    		if (citations[i] >= i + 1){
+    			h = i + 1;
+    		} else {
+    			break;
+    		}
+    	}
+    	return h;
+        
+    }
+};
