7149dcab2a08e62de7b1698fee5032edb461d952
c++junkie <asolinks@hotmail.com>
2025-06-05 21:54:07 +0300

    Insert Delete GetRandom O(1) Exercise - main function added

--- Delete_GetRandom.cpp
+++ Delete_GetRandom.cpp
@@ -40,3 +40,19 @@ public:
         return nums[randomIndex];
     }
 };
+
+int main() {
+    RandomizedSet randomizedSet;
+
+    std::cout << boolalpha; // Print bools as true/false
+
+    std::cout << "Insert 1: " << randomizedSet.insert(1) << std::endl;     // true
+    std::cout << "Remove 2: " << randomizedSet.remove(2) << std::endl;     // false
+    std::cout << "Insert 2: " << randomizedSet.insert(2) << std::endl;     // true
+    std::cout << "Get Random: " << randomizedSet.getRandom() << endl; // 1 or 2
+    std::cout << "Remove 1: " << randomizedSet.remove(1) << std::endl;     // true
+    std::cout << "Insert 2: " << randomizedSet.insert(2) << std::endl;     // false
+    std::cout << "Get Random: " << randomizedSet.getRandom() << std::endl; // 2
+
+    return 0;
+}
