09cc1ef3eb24e9553445c4d96455c0d69d652816
c++junkie <asolinks@hotmail.com>
2025-06-05 21:58:44 +0300

    bug modified and compiled

--- Delete_GetRandom.cpp
+++ Delete_GetRandom.cpp
@@ -1,3 +1,4 @@
+#include<iostream>
 #include <unordered_map>
 #include <vector>
 #include <cstdlib>
@@ -44,12 +45,12 @@ public:
 int main() {
     RandomizedSet randomizedSet;
 
-    std::cout << boolalpha; // Print bools as true/false
+    std::cout << std::boolalpha; // Print bools as true/false
 
     std::cout << "Insert 1: " << randomizedSet.insert(1) << std::endl;     // true
     std::cout << "Remove 2: " << randomizedSet.remove(2) << std::endl;     // false
     std::cout << "Insert 2: " << randomizedSet.insert(2) << std::endl;     // true
-    std::cout << "Get Random: " << randomizedSet.getRandom() << endl; // 1 or 2
+    std::cout << "Get Random: " << randomizedSet.getRandom() << std::endl; // 1 or 2
     std::cout << "Remove 1: " << randomizedSet.remove(1) << std::endl;     // true
     std::cout << "Insert 2: " << randomizedSet.insert(2) << std::endl;     // false
     std::cout << "Get Random: " << randomizedSet.getRandom() << std::endl; // 2
