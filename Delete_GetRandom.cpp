845478b550b444f7fd448b6ee5fba46121cc7306
c++junkie <asolinks@hotmail.com>
2025-06-05 21:36:17 +0300

    Insert Delete GetRandom O(1) Exercise

---
+++ Delete_GetRandom.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+class RandomizedSet {
+public:
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        
+    }
+    
+    bool remove(int val) {
+        
+    }
+    
+    int getRandom() {
+        
+    }
+};
