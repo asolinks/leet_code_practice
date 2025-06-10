98663f44531faf9ad6eeda3f2e71f71972ad8f98
c++junkie <asolinks@hotmail.com>
2025-06-10 20:43:40 +0300

    Integer to roman exercise - Solved

---
+++ integer_to_roman.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    std::string intToRoman(int num) {
+        std::vector<std::pair<int, std::string>> roman = {
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"}
+        };
+
+        std::string output;
+        for (const auto& [value, symbol] : roman){
+            while (num >= value){
+                output += symbol;
+                num -= value;
+            }
+        }
+        return output;
+        
+    }
+};
