da8b15b15018b213b6c6fad924239e7644ae14f7
c++junkie <asolinks@hotmail.com>
2025-06-11 20:55:27 +0300

    Longest Common Prefix - main function added

--- lcp.cpp
+++ lcp.cpp
@@ -19,3 +19,29 @@ public:
         return prefix;
     }
 };
+
+int main() {
+    Solution solution;
+
+    // Test Case 1
+    std::vector<std::string> strs1 = {"flower", "flow", "flight"};
+    std::cout << "Common Prefix: " << solution.longestCommonPrefix(strs1) << std::endl;
+
+    // Test Case 2
+    std::vector<std::string> strs2 = {"dog", "racecar", "car"};
+    std::cout << "Common Prefix: " << solution.longestCommonPrefix(strs2) << std::endl;
+
+    // Test Case 3
+    std::vector<std::string> strs3 = {"interspecies", "interstellar", "interstate"};
+    std::cout << "Common Prefix: " << solution.longestCommonPrefix(strs3) << std::endl;
+
+    // Test Case 4
+    std::vector<std::string> strs4 = {""};
+    std::cout << "Common Prefix: " << solution.longestCommonPrefix(strs4) << std::endl;
+
+    // Test Case 5
+    std::vector<std::string> strs5 = {"a"};
+    std::cout << "Common Prefix: " << solution.longestCommonPrefix(strs5) << std::endl;
+
+    return 0;
+}
