4445b82605d477015e9a4f670bb332c217f7edb1
c++junkie <asolinks@hotmail.com>
2025-06-17 22:06:11 +0300

    Is Subsequence Exercise - main function added

--- is_subsequence.cpp
+++ is_subsequence.cpp
@@ -16,3 +16,27 @@ public:
         
     }
 };
+
+int main() {
+    Solution solution;
+
+    // Test case 1
+    std::string s1 = "abc";
+    std::string t1 = "ahbgdc";
+    std::cout << "Input: s = \"" << s1 << "\", t = \"" << t1 << "\"\n";
+    std::cout << "Output: " << (solution.isSubsequence(s1, t1) ? "true" : "false") << "\n\n";
+
+    // Test case 2
+    std::string s2 = "axc";
+    std::string t2 = "ahbgdc";
+    std::cout << "Input: s = \"" << s2 << "\", t = \"" << t2 << "\"\n";
+    std::cout << "Output: " << (solution.isSubsequence(s2, t2) ? "true" : "false") << "\n\n";
+
+    // Custom test case
+    std::string s3 = "ace";
+    std::string t3 = "abcde";
+    std::cout << "Input: s = \"" << s3 << "\", t = \"" << t3 << "\"\n";
+    std::cout << "Output: " << (solution.isSubsequence(s3, t3) ? "true" : "false") << "\n";
+
+    return 0;
+}
