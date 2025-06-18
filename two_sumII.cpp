2a7cbf91c8e5b9c43db3bd6a4acc3bf57ece9b75
c++junkie <asolinks@hotmail.com>
2025-06-18 19:47:06 +0300

    Two Sum II - Input Array Is Sorted Exercise - solved@

---
+++ two_sumII.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return {left + 1, right + 1};  // 1-indexed
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return {};  
+    }
+};
