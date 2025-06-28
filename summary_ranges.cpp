43bc5128a6d81ffe821519658f2ce0f9404fd3aa
Stanley Amaechi <stanley.amaechi@student.oulu.fi>
2025-06-28 19:24:43 +0300

    Summary Ranges Exercise - Solved

--- summary_ranges.cpp
+++ summary_ranges.cpp
@@ -5,6 +5,25 @@
 class Solution {
 public:
     std::vector<std::string> summaryRanges(std::vector<int>& nums) {
+        std::vector<std::string> output;
+        if (nums.empty()) return output;
         
+        int left = 0;
+        
+        for (int i = 1; i <= nums.size(); ++i) {
+           
+            if (i == nums.size() || nums[i] != nums[i - 1] + 1) {
+                if (i - 1 == left) {
+                    
+                    output.push_back(std::to_string(nums[left]));
+                } else {
+                    
+                    output.push_back(std::to_string(nums[left]) + "->" + std::to_string(nums[i - 1]));
+                }
+                left = i; 
+            }
+        }
+        
+        return output;
     }
 };
