10d6cd96cf2cad735faf071db4b4e929f3827771
c++junkie <asolinks@hotmail.com>
2025-07-01 23:22:35 +0300

    Sqrt(x) Exercise - Solved

--- sqrt_x.cpp
+++ sqrt_x.cpp
@@ -1,6 +1,15 @@
+#include<iostream>
+
+
 class Solution {
 public:
     int mySqrt(int x) {
-        
+        if ( x < 2) return x;
+        int i = 1;
+
+        while ((long long) i * i <= x){
+            ++i;
+        } 
+        return i - 1;       
     }
 };
