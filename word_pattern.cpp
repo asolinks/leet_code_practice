c2c0eeb5347074a7797b5c67eefe8c0aabda2fc3
c++junkie <asolinks@hotmail.com>
2025-07-05 22:35:46 +0300

    Word Pattern

---
+++ word_pattern.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <sstream>
+
+class Solution {
+public:
+    bool wordPattern(std::string pattern, std::string s) {
+        std::vector<std::string> words;
+        std::string word;
+        std::istringstream iss(s);
+
+        while (iss >> word) {
+            words.push_back(word);
+        }
+        
+        if (words.size() != pattern.size()) {
+            return false;
+        }
+
+        std::unordered_map<char, std::string> charToWord;
+        std::unordered_map<std::string, char> wordToChar;
+
+        for (int i = 0; i < pattern.size(); ++i) {
+            char c = pattern[i];
+            std::string w = words[i];
+
+            if (charToWord.count(c)) {
+                if (charToWord[c] != w) return false;
+            } else {
+                charToWord[c] = w;
+            }
+
+            if (wordToChar.count(w)) {
+                if (wordToChar[w] != c) return false;
+            } else {
+                wordToChar[w] = c;
+            }
+        }
+        
+        return true;
+    }
+};
