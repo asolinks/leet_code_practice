3214c27467bb1f2e74c307b05d8e140f931cb2d1
c++junkie <asolinks@hotmail.com>
2025-07-09 22:29:03 +0300

    Two Sum Exercise - Main function added

--- two_sum.cpp
+++ two_sum.cpp
@@ -22,3 +22,21 @@ public:
         
     
 };
+
+
+int main() {
+    Solution solution;
+
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    std::vector<int> result = solution.twoSum(nums, target);
+
+    if (!result.empty()) {
+        std::cout << "Indices: " << result[0] << ", " << result[1] << std::endl;
+    } else {
+        std::cout << "No two sum solution found." << std::endl;
+    }
+
+    return 0;
+}
