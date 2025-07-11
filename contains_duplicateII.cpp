b6ea83ada3c0048179cc9738cef4d7cae95c99a6
c++junkie <asolinks@hotmail.com>
2025-07-11 23:32:56 +0300

    Contains Duplicate II Exercise - Solved

---
+++ contains_duplicateII.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        std::unordered_set<int> window;
+
+        for (int i = 0; i < nums.size(); ++i){
+            if (window.count(num[i])) return true;
+            window.insert(nums[i]);
+
+            if (window.size() > k){
+                window.erase(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+};
