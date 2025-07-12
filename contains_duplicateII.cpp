cacc2d5191d1d9b7fa2e0c9643879bf682b35a98
c++junkie <asolinks@hotmail.com>
2025-07-12 16:11:09 +0300

    compiled

--- contains_duplicateII.cpp
+++ contains_duplicateII.cpp
@@ -4,7 +4,7 @@
 
 class Solution {
 public:
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+    bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
         std::unordered_set<int> window;
 
         for (int i = 0; i < nums.size(); ++i){
@@ -23,17 +23,17 @@ public:
 int main() {
     Solution solution;
 
-    vector<int> nums1 = {1, 2, 3, 1};
+    std::vector<int> nums1 = {1, 2, 3, 1};
     int k1 = 3;
-    cout << "Test Case 1: " << (solution.containsNearbyDuplicate(nums1, k1) ? "true" : "false") << endl;
+    std::cout << "Test Case 1: " << (solution.containsNearbyDuplicate(nums1, k1) ? "true" : "false") << std::endl;
 
-    vector<int> nums2 = {1, 0, 1, 1};
+    std::vector<int> nums2 = {1, 0, 1, 1};
     int k2 = 1;
-    cout << "Test Case 2: " << (solution.containsNearbyDuplicate(nums2, k2) ? "true" : "false") << endl;
+    std::cout << "Test Case 2: " << (solution.containsNearbyDuplicate(nums2, k2) ? "true" : "false") << std::endl;
 
-    vector<int> nums3 = {1, 2, 3, 4, 5};
+    std::vector<int> nums3 = {1, 2, 3, 4, 5};
     int k3 = 2;
-    cout << "Test Case 3: " << (solution.containsNearbyDuplicate(nums3, k3) ? "true" : "false") << endl;
+    std::cout << "Test Case 3: " << (solution.containsNearbyDuplicate(nums3, k3) ? "true" : "false") << std::endl;
 
     return 0;
 }
