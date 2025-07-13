d3f84f356e4ef322fbc05b8a83ac01f80a2ba688
c++junkie <asolinks@hotmail.com>
2025-07-13 22:24:27 +0300

    Longest Consecutive Sequence Exercise - solved

---
+++ longest_cons.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+
+class Solution {
+public:
+    int longestConsecutive(std::vector<int>& nums) {
+        std::unordered_set<int> numSet(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int num : numSet) {
+            if (numSet.find(num - 1) == numSet.end()) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.find(currentNum + 1) != numSet.end()) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longest = std::max(longest, currentStreak);
+            }
+        }
+
+        return longest;
+    }
+};
